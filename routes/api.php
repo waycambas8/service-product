84f711145988f1652b4aab000d50ad3024a945be
waycambas8 <waycambas8@gmail.com>
2024-07-18 15:49:08 +0700

    init

---
+++ routes/api.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+$Service = 'product';
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('', function () {
+    return response()->json([
+        'msg' => 'Beep Beep Service Product is online'
+    ]);
+});
+
+Route::group([
+    'prefix' => "$Service"
+], function ($router) {
+});
